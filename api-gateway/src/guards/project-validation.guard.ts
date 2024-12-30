aabb4f738fa4d0d6356496d4c1ef223f0f28b923
leonardo <quevedoruiz41@gmail.com>
2024-12-30 17:33:02 -0500

    Create a guard to validate if a project exists before some tasks actions

---
+++ api-gateway/src/guards/project-validation.guard.ts
@@ -0,0 +1,48 @@
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import {
+  CanActivate,
+  ExecutionContext,
+  HttpException,
+  HttpStatus,
+  Inject,
+  Injectable,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Cache } from 'cache-manager';
+import { Request } from 'express';
+
+@Injectable()
+export class ProjectValidationGuard implements CanActivate {
+  constructor(
+    @Inject(CACHE_MANAGER)
+    private readonly cacheManager: Cache,
+    private readonly reflector: Reflector,
+  ) {}
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const exlcudeGuard = this.reflector.get<boolean>(
+      'excludeProjectvalidationGurad',
+      context.getHandler(),
+    );
+
+    if (exlcudeGuard) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest<Request>();
+    const { project_code } = request.params;
+    if (!project_code) {
+      throw new HttpException(
+        'Project project_code is required',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    const cacheKey = `project:${project_code}`;
+    const project = await this.cacheManager.get(cacheKey);
+
+    if (!project) {
+      throw new HttpException('Project not found', HttpStatus.NOT_FOUND);
+    }
+
+    return true;
+  }
+}
--- api-gateway/src/tasks/tasks.controller.ts
+++ api-gateway/src/tasks/tasks.controller.ts
@@ -7,31 +7,42 @@ import {
   Param,
   Delete,
   Inject,
+  UseGuards,
+  SetMetadata,
 } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
 import { TASK_MICROSERVICE_KEY } from './services/tasks.services';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { catchError } from 'rxjs';
+import { ProjectValidationGuard } from 'src/guards/project-validation.guard';
 
+@UseGuards(ProjectValidationGuard)
 @Controller('tasks')
 export class TasksController {
   constructor(
     @Inject(TASK_MICROSERVICE_KEY) private readonly tasksCliente: ClientProxy,
   ) {}
 
-  @Post()
-  create(@Body() createTaskDto: CreateTaskDto) {
-    return this.tasksCliente.send({ cmd: 'createTask' }, createTaskDto).pipe(
+  @Post(':project_code')
+  create(
+    @Param('project_code') project_code: string,
+    @Body() createTaskDto: CreateTaskDto,
+  ) {
+    const taskData = {
+      projectCode: project_code,
+      createTaskDto,
+    };
+    return this.tasksCliente.send({ cmd: 'createTask' }, taskData).pipe(
       catchError((error) => {
         throw new RpcException(error);
       }),
     );
   }
 
+  @SetMetadata('excludeProjectvalidationGurad', true)
   @Get()
   findAll() {
-    console.log('api-gateway controler findall');
     return this.tasksCliente.send({ cmd: 'findAllTasks' }, {}).pipe(
       catchError((error) => {
         throw new RpcException(error);
@@ -39,6 +50,7 @@ export class TasksController {
     );
   }
 
+  @SetMetadata('excludeProjectvalidationGurad', true)
   @Get(':code')
   findOne(@Param('code') code: string) {
     return this.tasksCliente.send({ cmd: 'findOneTask' }, { code }).pipe(
@@ -48,11 +60,16 @@ export class TasksController {
     );
   }
 
-  @Patch(':code')
-  update(@Param('code') code: string, @Body() updateTaskDto: UpdateTaskDto) {
+  @Patch(':project_code/:code')
+  update(
+    @Param('project_code') projecCode: string,
+    @Param('code') code: string,
+    @Body() updateTaskDto: UpdateTaskDto,
+  ) {
     const data = {
       ...updateTaskDto,
       code,
+      projecCode,
     };
     return this.tasksCliente.send({ cmd: 'updateTask' }, data).pipe(
       catchError((error) => {
@@ -61,8 +78,11 @@ export class TasksController {
     );
   }
 
-  @Delete(':code')
-  remove(@Param('code') code: string) {
+  @Delete(':project_code/:code')
+  remove(
+    @Param('project_code') projectCode: string,
+    @Param('code') code: string,
+  ) {
     return this.tasksCliente.send({ cmd: 'removeTask' }, { code }).pipe(
       catchError((error) => {
         throw new RpcException(error);
